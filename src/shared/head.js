2a9ce2c74b4b5cc768cc3d22b910122c45bf6683
shrivitthal <shrivitthalhindumandir@gmail.com>
2019-06-17 19:45:14 -0600

    commit

--- src/shared/head.js
+++ src/shared/head.js
@@ -2,7 +2,7 @@ import React from "react"
 import Helmet from "react-helmet"
 export default () =>
     <Helmet>
-       <link rel="apple-touch-icon" sizes="57x57" href="/apple-icon-57x57.png"/>
+        <link rel="apple-touch-icon" sizes="57x57" href="/apple-icon-57x57.png"/>
         <link rel="apple-touch-icon" sizes="60x60" href="/apple-icon-60x60.png"/>
         <link rel="apple-touch-icon" sizes="72x72" href="/apple-icon-72x72.png"/>
         <link rel="apple-touch-icon" sizes="76x76" href="/apple-icon-76x76.png"/>
@@ -19,4 +19,21 @@ export default () =>
         <meta name="msapplication-TileColor" content="#ffffff"/>
         <meta name="msapplication-TileImage" content="/ms-icon-144x144.png"/>
         <meta name="theme-color" content="#ffffff"/>
+        <meta charset="UTF-8"/>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+        <meta property="og:locale" content="en_US" />
+        <title>Discover Vietnam's Con Dao Islands - Official Travel and Adventure Guide</title>
+        <meta name="description" content="Visit Vietnam's Con Dao Islands and experience one of South East Asia's hidden gems.  Plan your trip itinerary by reading the official travel guide to this unique destination."/>
+        <meta name="original-source" content="https://www.condaoislandguide.com" />
+        <link rel="canonical" href="https://www.condaoislandguide.com" />
+        <meta property="og:title" content="A Guide To The Con Dao Islands In Vietnam"/>
+        <meta property="og:description" content="Ultimate Con Dao Islands Vietnam Travel &amp; Tourism Guide To The Best Hotels/Resorts, Beaches, Diving, Snorkeling, Turtle Viewing &amp; Prisons in Vietnam." />
+        <meta property="og:url" content="https://www.condaoislandguide.com"/>
+        <meta property="og:site_name" content="Con Dao Islands" />
+        <meta property="article:publisher" content="https://www.facebook.com/condaoislandguide/" />
+        <meta property="og:image:alt" content="Con Dao Island Vietnam Travel Guide" />
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta name="twitter:description" content="Ultimate Con Dao Islands Vietnam Travel &amp; Tourism Guide To The Best Hotels/Resorts, Beaches, Diving, Snorkeling, Turtle Viewing &amp; Prisons in Vietnam." />
+        <meta name="twitter:title" content="A Guide To The Con Dao Islands In Vietnam" />
+        <meta name="twitter:site" content="@phu_quoc_guide" />
     </Helmet>
